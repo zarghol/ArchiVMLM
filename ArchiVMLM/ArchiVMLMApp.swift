f14088e3c6bd9730fdc591e487e975756a894ad7
Clément Nonn <clement.nonn@worldline.com>
2021-03-12 09:46:40 +0100

    Initial Commit

---
+++ ArchiVMLM/ArchiVMLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiVMLMApp.swift
+//  ArchiVMLM
+//
+//  Created by Clément Nonn on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ArchiVMLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchiVMLM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArchiVMLM
+//
+//  Created by Clément Nonn on 12/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
